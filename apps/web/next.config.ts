b0174d2c5272d3dd22cf7863fcbaedad59656388
Jayavardhan Reddy <jayavardhanvijaya@gmail.com>
2026-04-13 09:43:47 +0530

    monday

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: false,
+  experimental: {
+    turbo: {
+      root: path.resolve(__dirname, "../.."),
+    },
+  },
 };
 
 export default nextConfig;
